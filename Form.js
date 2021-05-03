1bb0d2bd7f0dfd58bb1a32e5db4f1ea1529a6fd4
jaya-design <65601436+jaya-design@users.noreply.github.com>
2021-05-03 17:18:32 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,12 @@
+class Form{
+    constructor(){}
+    display(){
+ 
+        var button = createButton("Feed The Dog");
+        button.position(400,300)
+
+        var button2 = createButton("Add Food")
+        button2.position(200,300)
+        
+    }
+}
